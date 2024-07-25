e60965f95c0038b6eaf74de66de69b9de934fc08
siwakorn.k <siwakorn.k@blendata.co>
2024-07-25 16:36:57 +0700

    update after interview

--- index.js
+++ index.js
@@ -13,20 +13,35 @@ const prices = {
     "Yellow set": 50,
     "Pink set": 80,
     "Purple set": 90,
-    "Orange set": 120
+    "Orange set": 120,
+    "Black set": 200
 };
 
 function calculatePrice(orders, memberCard) {
     let total = 0;
-    const discounts = { "Orange set": 0.05, "Pink set": 0.05, "Green set": 0.05 };
-
+    const discounts = { "Orange set": 0.05, "Pink set": 0.05, "Green set": 0.05, };
+    const discountBlack = {"Black set": 0.2};
     for (const item in orders) {
         const quantity = orders[item];
-        if (discounts[item]) {
-            const bundles = Math.floor(quantity / 2);
-            const nonBundleQuantity = quantity % 2;
-            total += (bundles * 2 * prices[item] * (1 - discounts[item])) + (nonBundleQuantity * prices[item]);
-        } else {
+        if (discounts[item] || discountBlack[item]) {
+            if(discounts[item]){
+                const bundles = Math.floor(quantity / 2);
+                const nonBundleQuantity = quantity % 2;
+                total += (bundles * 2 * prices[item] * (1 - discounts[item])) + (nonBundleQuantity * prices[item]);
+            }
+            else if(discountBlack[item]){
+            const bundlesblack = Math.floor(quantity / 3);
+            const nonBundleQuantityblack = quantity % 3;
+            total += (bundlesblack * 3 * prices[item] * (1 - discountBlack[item])) + (nonBundleQuantityblack * prices[item]);
+            // total += (bundles * 2 * prices[item] * (1 - discounts[item])) + (nonBundleQuantity * prices[item]);
+            }
+        }
+        // else if(discountBlack[item]){
+                // const bundles = Math.floor(quantity / 3);
+                // const nonBundleQuantity = quantity % 3;
+                // total += (bundles * 3 * prices[item] * (1 - discountBlack[item])) + (nonBundleQuantity * prices[item]);
+        //     }
+        else {
             total += quantity * prices[item];
         }
     }
